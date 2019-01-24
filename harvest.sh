1601385b0f30928438cae0e5963087367b07c53b
Twan Goosen <twan@clarin.eu>
2019-01-24 17:02:39 +0100

    test harvest script

---
+++ harvest.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+SCRIPT_DIR=${PWD}
+HARVESTER_IMAGE="registry.gitlab.com/clarin-eric/docker-oai-harvester:0.0.1-rc9"
+RESOURCES_DIR="${SCRIPT_DIR}/resources"
+OUTPUT_DIR="${SCRIPT_DIR}/output"
+
+CONFIG_FILE="${SCRIPT_DIR}/config.xml"
+cp "${CONFIG_FILE}" "${RESOURCES_DIR}/config-test-test.xml"
+
+
+docker run \
+	--rm --name oai-test-harvester \
+	-v ${OUTPUT_DIR}:/app/workdir \
+	-v ${RESOURCES_DIR}:/app/oai/resources \
+	${HARVESTER_IMAGE} -n test -o test
+
