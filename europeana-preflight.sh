7204444c5eaa52aa85c25594937724f7984211ca
Twan Goosen <twan@clarin.eu>
2019-11-07 10:22:49 +0100

    test configs

--- europeana-preflight.sh
+++ europeana-preflight.sh
@@ -8,6 +8,20 @@ if [ -d "$OUT_DIR_BASE" ]; then
 fi
 
 filter_collections() {
+	filter_collection 9200114_Ag_EU_TEL_a0516_Bulgaria_Manuscript --media --text
+	filter_collection 92068_Ag_Slovenia_ETravel --media --text --open
+	filter_collection 2022411_Ag_RO_Elocal_audioinb
+	filter_collection 2022402_Ag_RO_Elocal_arhivele
+
+	filter_collection 0940429_Ag_PL_www.bibliotekacyfrowa.pl --media --text --open
+	filter_collection 2020125_Ag_SI_Nat.EContentAgg_sigic_articles --media --fulltext --open 
+	filter_collection 2020127_Ag_SI_Nat.EContentAgg_Books --media --fulltext --open
+	filter_collection 0940442_Ag_PL_bibliotekacyfrowa.eu --media --fulltext --open
+	filter_collection 92078_Ag_EU_TEL_a1030_EuropanaRegia --text --open
+	#filter_collection 92004_NL_Manuscriptorium_CZ
+	#filter_collection 9200519_Ag_BnF_Gallica_typedoc_manuscrits --text
+	#filter_collection 92065_Ag_EU_TEL_a0445_ETravel --media --text --open
+
   while read C; do filter_collection "$C" --open --media --fulltext; done <<EOF 
 0940431_Ag_PL_rcin.org.pl
 0940433_Ag_PL_mbc.malopolska.pl
@@ -15,6 +29,13 @@ filter_collections() {
 2022410_Ag_RO_Elocal_documen1
 9200394_Ag_Slovenia_Laibacher_Zeitung
 9200452_UL_Old_Rare_Books_Lucian_Blaga
+9200453_UL_Manuscripts_Lucian_Blaga
+9200456_UL_Reprints_Lucian_Blaga
+9200467_Ag_TEL_a1188_eCloud_Debrecen
+9200470_UL_Periodicals_Lucian_Blaga
+9200498_Ag_BnF_occitanica
+92054_Ag_Slovenia_Dom_in_svet
+92055_Ag_Slovenia_Ljubljanski_zvon
 EOF
 
 #  filter_collection "0940442_Ag_PL_bibliotekacyfrowa.eu" --open --media --fulltext
--- harvest.sh
+++ harvest.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 SCRIPT_DIR=${PWD}
+CONTAINER_NAME="oai-test-harvester"
 HARVESTER_IMAGE="registry.gitlab.com/clarin-eric/docker-oai-harvester:0.0.1-rc12"
 RESOURCES_DIR="${SCRIPT_DIR}/resources"
 ASSETS_DIR="${SCRIPT_DIR}/assets"
@@ -11,7 +12,7 @@ cp "${CONFIG_FILE}" "${RESOURCES_DIR}/config-test-test.xml"
 
 
 docker run \
-	--rm --name oai-test-harvester \
+	--rm --name ${CONTAINER_NAME} \
 	-v ${OUTPUT_DIR}:/app/workdir \
 	-v ${RESOURCES_DIR}:/app/oai/resources \
 	-v ${ASSETS_DIR}:/app/oai/resources/assets \
