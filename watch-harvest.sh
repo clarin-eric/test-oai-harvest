85f5e7212d8be239f33e8c711bebd2720d88cdc3
Twan Goosen <twan@clarin.eu>
2020-01-21 15:59:40 +0100

    preflight + harvest sets

--- watch-harvest.sh
+++ watch-harvest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 NAME="${1:-Europeana}"
-OUTPUT_DIR="${PWD}/output3"
+OUTPUT_DIR="${PWD}/output"
 watch -d -n 5 "find ${OUTPUT_DIR}/workdir/test-test/oai-pmh -type f|wc -l && find ${OUTPUT_DIR}/workdir/test-test/results -type f|wc -l && tail -n 5 ${OUTPUT_DIR}/workdir/test-test/log/${NAME}.log && tree -ths ${OUTPUT_DIR}/workdir/test-test/results | head -n 25"
