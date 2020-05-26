3023b744be9caac25d1e832c7d0d977b0a985922
Twan Goosen <twan@clarin.eu>
2020-05-26 17:12:14 +0200

    harvester version

--- harvest.sh
+++ harvest.sh
@@ -2,7 +2,8 @@
 
 SCRIPT_DIR=${PWD}
 CONTAINER_NAME="oai-test-harvester"
-HARVESTER_IMAGE="registry.gitlab.com/clarin-eric/docker-oai-harvester:0.0.1-rc12.4"
+HARVESTER_IMAGE="registry.gitlab.com/clarin-eric/docker-oai-harvester:1.1.0"
+#HARVESTER_IMAGE="docker-oai-harvester:1.0.0-4-gbdefd2a"
 RESOURCES_DIR="${SCRIPT_DIR}/resources"
 ASSETS_DIR="${SCRIPT_DIR}/assets"
 OUTPUT_DIR="${SCRIPT_DIR}/output"
@@ -13,7 +14,10 @@ else
 	echo "Usage: $0 <config file>" >&2
 	exit 1
 fi
-cp "${CONFIG_FILE}" "${RESOURCES_DIR}/config-test-test.xml"
+
+if ! cp "${CONFIG_FILE}" "${RESOURCES_DIR}/config-test-test.xml"; then
+	exit 1
+fi
 
 
 docker run \
@@ -21,5 +25,6 @@ docker run \
 	-v ${OUTPUT_DIR}:/app/workdir \
 	-v ${RESOURCES_DIR}:/app/oai/resources \
 	-v ${ASSETS_DIR}:/app/oai/resources/assets \
+        -v ${ASSETS_DIR}/expandMap.xsl:/app/oai/expandMap.xsl \
 	${HARVESTER_IMAGE} -n test -o test
 
