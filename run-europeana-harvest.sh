29ed98139b57fa70a72a5c5d2cb66e072840f516
Twan Goosen <twan@clarin.eu>
2020-05-26 17:42:14 +0200

    run europeana script updated

--- run-europeana-harvest.sh
+++ run-europeana-harvest.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 set -e
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 LOGFILE=europeana-harvest.log
 (	
-	cd ~/test-harvest
+	cd ${BASE_DIR}
 	echo "$(date) Starting preflight" | tee -a $LOGFILE
 	./europeana-preflight.sh > assets/selection.xml 2>> $LOGFILE
 	echo "$(date) Starting harvest" | tee -a $LOGFILE
-	./harvest.sh config-europeana-filtered.xml >> $LOGFILE
+	./harvest.sh europeana-filtered >> $LOGFILE
 )
 
 
